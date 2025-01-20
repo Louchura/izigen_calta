17ed6221b48abfe592a34316d1c5666b89df95e0
HU <h-uchiumi0627@outlook.jp>
2025-01-20 23:59:21 +0900

    作業復旧分

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
     public Button quitButton; // 終了ボタン
 
     private int correctIndex; // 正解の手札のインデックス
-    private float timeLimit = 10f; // 制限時間（秒）
+    private float timeLimit = 3f; // 制限時間（秒）
     private float remainingTime; // 残り時間
     private bool isTimeRunning = false; // タイマーの状態を管理
 
@@ -191,6 +191,7 @@ Debug.Log($"CSVから{cardDatabase.Count}枚のカードデータを読み込み
         isTimeRunning = false;
         timerText.gameObject.SetActive(false); // 制限時間の表示を消す
         Debug.Log("時間切れ！");
+        ShowChoicePanel();
     }
 
     // カードがクリックされたときの処理
